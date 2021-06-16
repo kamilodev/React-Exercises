5b7cdd895c5377b187a807420541b61e4e31d771
Camilo Vanegas <kamilo_vanegas@hotmail.com>
2021-06-16 08:12:17 +0200

    Test GiftExpertApp Component Finished

--- 04-gif-expert-app/src/components/GiftGrid.js
+++ 04-gif-expert-app/src/components/GiftGrid.js
@@ -1,22 +1,20 @@
-import React from 'react'
-import { useFetchGifts } from '../hooks/useFetchGifts'
-import GiftGridItem from './GiftGridItem'
+import React from 'react';
+import { useFetchGifts } from '../hooks/useFetchGifts';
+import GiftGridItem from './GiftGridItem';
 
 export const GiftGrid = ({ category }) => {
-	const { data: images, loading } = useFetchGifts(category)
+	const { data: images, loading } = useFetchGifts(category);
 
 	return (
 		<>
-			<h3 className=' animate__animated animate__fadeIn'> {category} </h3>
-			{loading && (
-				<p className=' animate__animated animate__flash'>Loading</p>
-			)}
+			<h3 className=" animate__animated animate__fadeIn"> {category} </h3>
+			{loading && <p className=" animate__animated animate__flash">Loading</p>}
 			<br />
-			<div className='card-grid'>
-				{images.map(img => (
+			<div className="card-grid">
+				{images.map((img) => (
 					<GiftGridItem key={img.id} {...img} />
 				))}
 			</div>
 		</>
-	)
-}
+	);
+};
--- 04-gif-expert-app/src/tests/components/GiftGrid.test.js
+++ 04-gif-expert-app/src/tests/components/GiftGrid.test.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import { GiftGrid } from '../../components/GiftGrid'
-import { shallow } from 'enzyme'
-import { useFetchGifts } from '../../hooks/useFetchGifts'
-jest.mock('../../hooks/useFetchGifts')
+import React from 'react';
+import { GiftGrid } from '../../components/GiftGrid';
+import { shallow } from 'enzyme';
+import { useFetchGifts } from '../../hooks/useFetchGifts';
+import '@testing-library/jest-dom';
+jest.mock('../../hooks/useFetchGifts');
 
 describe('Tests for GiftGrid component', () => {
-	const category = 'Goku'
+	const category = 'Goku';
 	// const wrapper = shallow(<GifGrid category={category}/>);
 
 	test('should show the correct component and match with the snapshot', () => {
 		useFetchGifts.mockReturnValue({
 			data: [],
 			loading: true,
-		})
-		const wrapper = shallow(<GiftGrid category={category} />)
-		expect(wrapper).toMatchSnapshot()
-	})
+		});
+		const wrapper = shallow(<GiftGrid category={category} />);
+		expect(wrapper).toMatchSnapshot();
+	});
 
 	test('should change the snapshot when useFetchGifs is executed', () => {
 		const gifs = [
@@ -24,15 +25,21 @@ describe('Tests for GiftGrid component', () => {
 				url: 'https://google.com',
 				title: 'ABC Title',
 			},
-		]
+			{
+				id: '123',
+				url: 'https://google.com',
+				title: 'ABC Title',
+			},
+		];
 
 		useFetchGifts.mockReturnValue({
 			data: gifs,
 			loading: false,
-		})
+		});
 
-		const wrapper = shallow(<GiftGrid category={category} />)
-
-		expect(wrapper).toMatchSnapshot()
-	})
-})
+		const wrapper = shallow(<GiftGrid category={category} />);
+		// expect(wrapper).toMatchSnapshot();
+		expect(wrapper.find('p').exists()).toBe(false);
+		expect(wrapper.find('GiftGridItem').length).toBe(gifs.length);
+	});
+});
