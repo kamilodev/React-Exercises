6cc2756d7a4d03fad44528604f5c363c2d7c2278
kamilo923 <kamilo_vanegas@hotmail.com>
2021-06-22 19:19:48 +0200

    Simple Form UseEffect Created

--- 05-hook-app/src/components/01-useState/CounterWithCustomHook.js
+++ 05-hook-app/src/components/01-useState/CounterWithCustomHook.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { useCounter } from '../../hooks/useCounter';
-import './counter.css';
+import React from 'react'
+import { useCounter } from '../../hooks/useCounter'
+import './counter.css'
 
 export const CounterWithCustomHook = () => {
-	const { state, increment, decrement, reset } = useCounter(100);
+	const { state, increment, decrement, reset } = useCounter(100)
 
 	return (
 		<>
 			<h1>Counter With Hook {state} </h1>
 			<hr />
 
-			<button onClick={() => increment(2)} className="btn btn-primary">
+			<button onClick={() => increment(1)} className='btn btn-primary'>
 				+ 1
 			</button>
-			<button onClick={() => decrement(1)} className="btn btn-primary">
+			<button onClick={() => decrement(1)} className='btn btn-primary'>
 				- 1
 			</button>
-			<button onClick={reset} className="btn btn-danger">
+			<button onClick={reset} className='btn btn-danger'>
 				Reset
 			</button>
 		</>
-	);
-};
+	)
+}
---
+++ 05-hook-app/src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react'
+import './effects.css'
+
+export const SimpleForm = () => {
+	const [formState, setFormState] = useState({
+		name: '',
+		email: '',
+	})
+
+	const { name, email } = formState
+
+	useEffect(() => {
+		console.log('Hey')
+	}, [])
+
+	useEffect(() => {
+		console.log('Form State Cambio')
+	}, [formState])
+
+	useEffect(() => {
+		console.log('Email Cambio')
+	}, [email])
+
+	const handleInputChange = ({ target }) => {
+		setFormState({
+			...formState,
+			[target.name]: target.value,
+		})
+	}
+
+	return (
+		<>
+			<h1>Use Effect</h1>
+			<hr />
+
+			<div className='form-group'>
+				<input
+					type='text'
+					name='name'
+					className='form-control'
+					placeholder='Tu Nombre'
+					autoComplete='off'
+					value={name}
+					onChange={handleInputChange}
+				/>
+			</div>
+
+			<div className='form-group'>
+				<input
+					type='text'
+					name='email'
+					className='form-control'
+					placeholder='email@email.com'
+					autoComplete='off'
+					value={email}
+					onChange={handleInputChange}
+				/>
+			</div>
+		</>
+	)
+}
