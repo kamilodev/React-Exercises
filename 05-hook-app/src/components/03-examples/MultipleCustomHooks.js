ba4247d1f36cc8b4ca136a8909f80614921d216c
kamilo923 <kamilo_vanegas@hotmail.com>
2021-06-22 22:10:49 +0200

    Creating MultipleCustomHooks

---
+++ 05-hook-app/src/components/03-examples/MultipleCustomHooks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../02-useEffect/effects.css'
+
+export const MultipleCustomHooks = () => {
+	return (
+		<div>
+			<h1>Custom Hooks!!</h1>
+			<hr />
+		</div>
+	)
+}
--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook'
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook'
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks'
 //import { CounterApp } from './components/01-useState/CounterApp'
 // import { HookApp } from './HookApp';
 
-ReactDOM.render(<FormWithCustomHook />, document.getElementById('root'))
+ReactDOM.render(<MultipleCustomHooks />, document.getElementById('root'))
