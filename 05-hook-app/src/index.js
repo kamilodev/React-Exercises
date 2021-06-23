feb62f7d0e6476813fbaa48540d5b02aff652623
kamilo923 <kamilo_vanegas@hotmail.com>
2021-06-23 18:55:02 +0200

    Starting With useRef

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { FocusScreen } from './components/04-useRef/FocusScreen'
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook'
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks'
+//import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks'
 //import { CounterApp } from './components/01-useState/CounterApp'
 // import { HookApp } from './HookApp';
 
-ReactDOM.render(<MultipleCustomHooks />, document.getElementById('root'))
+ReactDOM.render(<FocusScreen />, document.getElementById('root'))
