620ed4ab875aee924827c4d7eac54815de3fdc16
kamilo923 <kamilo_vanegas@hotmail.com>
2021-06-23 21:44:35 +0200

    Callback Hook Exercise

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { MemoHook } from './components/06-memos/MemoHook'
+import { CallbackHook } from './components/06-memos/CallbackHook'
+//import { MemoHook } from './components/06-memos/MemoHook'
 // import { FocusScreen } from './components/04-useRef/FocusScreen'
 //import { RealExampleRef } from './components/04-useRef/RealExampleRef'
 //import { Layout } from './components/05-useLayoutEffect/Layout'
@@ -10,4 +11,4 @@ import { MemoHook } from './components/06-memos/MemoHook'
 //import { CounterApp } from './components/01-useState/CounterApp'
 // import { HookApp } from './HookApp';
 
-ReactDOM.render(<MemoHook />, document.getElementById('root'))
+ReactDOM.render(<CallbackHook />, document.getElementById('root'))
