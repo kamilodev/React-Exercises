d412eb2564fe56a59a1a02e9c6d393e0e3b1d4c0
Camilo Vanegas <kamilo_vanegas@hotmail.com>
2021-06-23 13:12:22 +0200

    useFetch API Created

--- 05-hook-app/src/components/03-examples/MultipleCustomHooks.js
+++ 05-hook-app/src/components/03-examples/MultipleCustomHooks.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import '../02-useEffect/effects.css'
+import {useFetch} from "../../hooks/useFetch";
 
 export const MultipleCustomHooks = () => {
+
+	const state = useFetch('https://www.breakingbadapi.com/api/quotes/2')
+	console.log(state)
+
 	return (
 		<div>
 			<h1>Custom Hooks!!</h1>
