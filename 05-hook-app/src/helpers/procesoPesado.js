e4673eceae7e2613b5392aadb1a6476557e2a299
kamilo923 <kamilo_vanegas@hotmail.com>
2021-06-23 21:21:18 +0200

    useMemo Exercise

---
+++ 05-hook-app/src/helpers/procesoPesado.js
@@ -0,0 +1,6 @@
+export const procesoPesado = iteraciones => {
+	for (let i = 0; i < iteraciones; i++) {
+		console.log('Ahi Vamos...')
+	}
+	return `${iteraciones} iteraciones realizadas`
+}
--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { MemoHook } from './components/06-memos/MemoHook'
 // import { FocusScreen } from './components/04-useRef/FocusScreen'
 //import { RealExampleRef } from './components/04-useRef/RealExampleRef'
 //import { Layout } from './components/05-useLayoutEffect/Layout'
-import { Memorize } from './components/06-memos/Memorize'
+//import { Memorize } from './components/06-memos/Memorize'
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook'
 //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks'
 //import { CounterApp } from './components/01-useState/CounterApp'
 // import { HookApp } from './HookApp';
 
-ReactDOM.render(<Memorize />, document.getElementById('root'))
+ReactDOM.render(<MemoHook />, document.getElementById('root'))
