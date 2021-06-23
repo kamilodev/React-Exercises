50df772c3b6a6a6a87d2c43c7b2b1e384f7660c1
kamilo923 <kamilo_vanegas@hotmail.com>
2021-06-23 19:55:30 +0200

    useLayoutEffect Example

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import { FocusScreen } from './components/04-useRef/FocusScreen'
-import { RealExampleRef } from './components/04-useRef/RealExampleRef'
+//import { RealExampleRef } from './components/04-useRef/RealExampleRef'
+import { Layout } from './components/05-useLayoutEffect/Layout'
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook'
 //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks'
 //import { CounterApp } from './components/01-useState/CounterApp'
 // import { HookApp } from './HookApp';
 
-ReactDOM.render(<RealExampleRef />, document.getElementById('root'))
+ReactDOM.render(<Layout />, document.getElementById('root'))
