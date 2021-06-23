bb71eeb163cefe057d05b2718ea2afa6bb29e211
kamilo923 <kamilo_vanegas@hotmail.com>
2021-06-23 20:48:07 +0200

    React Memo Function

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // import { FocusScreen } from './components/04-useRef/FocusScreen'
 //import { RealExampleRef } from './components/04-useRef/RealExampleRef'
-import { Layout } from './components/05-useLayoutEffect/Layout'
+//import { Layout } from './components/05-useLayoutEffect/Layout'
+import { Memorize } from './components/06-memos/Memorize'
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook'
 //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks'
 //import { CounterApp } from './components/01-useState/CounterApp'
 // import { HookApp } from './HookApp';
 
-ReactDOM.render(<Layout />, document.getElementById('root'))
+ReactDOM.render(<Memorize />, document.getElementById('root'))
