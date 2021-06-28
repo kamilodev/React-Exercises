37a745338427d1820a5ac8d4cc6c12d7112778ec
kamilo923 <kamilo_vanegas@hotmail.com>
2021-06-28 22:41:50 +0200

    TODO App Finished

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -1,100 +1,113 @@
-import React, {useEffect, useReducer} from 'react';
-import {todoReducer} from './todoReducer'
-import {useForm} from "../../hooks/useForm";
+import React, { useEffect, useReducer } from 'react'
+import { todoReducer } from './todoReducer'
+import { useForm } from '../../hooks/useForm'
 import './styles.css'
 
-
 const init = () => {
-
-    return JSON.parse(localStorage.getItem('todos')) || []
+	return JSON.parse(localStorage.getItem('todos')) || []
 }
 
 const TodoApp = () => {
+	const [todos, dispatch] = useReducer(todoReducer, [], init)
 
-    const [todos, dispatch] = useReducer(todoReducer, [], init);
+	const [{ description }, handleInputChange, reset] = useForm({
+		description: '',
+	})
 
-    const [{description}, handleInputChange, reset] = useForm({
-        description: ''
-    })
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos))
+	}, [todos])
 
-    useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(todos))
-    }, [todos])
+	const handleDelete = todoId => {
+		const action = {
+			type: 'delete',
+			payload: todoId,
+		}
 
-    console.log(description)
-    console.log(todos)
+		dispatch(action)
+	}
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+	const handleToggle = todoId => {
+		dispatch({
+			type: 'toggle',
+			payload: todoId,
+		})
+	}
 
-        if(description.trim().length <=1){
-            return
-        }
+	const handleSubmit = e => {
+		e.preventDefault()
 
-        const newTodo = {
-            id: new Date().getTime(),
-            desc: description,
-            done: false
-        }
+		if (description.trim().length <= 1) {
+			return
+		}
 
-        const action = {
-            type: 'add',
-            payload: newTodo,
-        }
+		const newTodo = {
+			id: new Date().getTime(),
+			desc: description,
+			done: false,
+		}
 
-        dispatch(action)
-        reset()
-    }
+		const action = {
+			type: 'add',
+			payload: newTodo,
+		}
 
-    return (
-        <div>
-            <h1>TodoApp ({todos.length}) </h1>
-            <hr />
+		dispatch(action)
+		reset()
+	}
 
-            <div className='row'>
-                <div className='col-7'>
-                    <ul className='list-group list-group-flush'>
-                        {
-                            todos.map((todo, i) => (
-                                <li
-                                    key={todo.id}
-                                    className='list-group-item'
-                                >
-                                    <p className='text-center'>{i + 1}. {todo.desc}</p>
-                                    <button className='btn btn-danger'>
-                                        Borrar
-                                    </button>
-                                </li>
-                            ))}
-                    </ul>
-                </div>
-                <div className='col-5'>
-                    <h4>Agregar TODO</h4>
-                    <hr/>
+	return (
+		<div>
+			<h1>TodoApp ({todos.length}) </h1>
+			<hr />
 
-                    <form onSubmit={handleSubmit}>
-                        <input
-                            type="text"
-                            name='description'
-                            className='form-control'
-                            placeholder='Aprender ...'
-                            autoComplete='off'
-                            value={description}
-                            onChange={handleInputChange}
-                        />
-                        <br />
+			<div className='row'>
+				<div className='col-7'>
+					<ul className='list-group list-group-flush'>
+						{todos.map((todo, i) => (
+							<li key={todo.id} className='list-group-item'>
+								<p
+									className={`${todo.done && 'complete'}`}
+									onClick={() => handleToggle(todo.id)}>
+									{i + 1}. {todo.desc}
+								</p>
+								<button
+									className='btn btn-danger'
+									onClick={() => {
+										handleDelete(todo.id)
+									}}>
+									Borrar
+								</button>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className='col-5'>
+					<h4>Agregar TODO</h4>
+					<hr />
 
-                        <button
-                            type='submit'
-                            className='btn btn-primary w-100 mt-2'
-                        >
-                            Agregar +
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    );
-};
+					<form onSubmit={handleSubmit}>
+						<input
+							type='text'
+							name='description'
+							className='form-control'
+							placeholder='Aprender ...'
+							autoComplete='off'
+							value={description}
+							onChange={handleInputChange}
+						/>
+						<br />
 
-export default TodoApp;
+						<button
+							type='submit'
+							className='btn btn-primary w-100 mt-2'>
+							Agregar +
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default TodoApp
--- 05-hook-app/src/components/08-useReducer/todoReducer.js
+++ 05-hook-app/src/components/08-useReducer/todoReducer.js
@@ -1,12 +1,31 @@
-
-
 export const todoReducer = (state = [], action) => {
-    switch (action.type) {
+	switch (action.type) {
+		case 'add':
+			return [...state, action.payload]
 
-        case 'add':
-            return [...state, action.payload]
+		case 'delete':
+			return state.filter(todo => todo.id != action.payload)
 
-        default:
-            return state
-    }
+		case 'toggle':
+			return state.map(todo =>
+				todo.id === action.payload
+					? { ...todo, done: !todo.done }
+					: todo,
+			)
+
+		case 'toggle-old':
+			return state.map(todo => {
+				if (todo.id === action.payload) {
+					return {
+						...todo,
+						done: !todo.done,
+					}
+				} else {
+					return todo
+				}
+			})
+
+		default:
+			return state
+	}
 }
