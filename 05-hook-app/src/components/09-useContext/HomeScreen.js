be9346aca07d561bb533d08a104f92618dffa050
kamilo923 <kamilo_vanegas@hotmail.com>
2021-06-29 21:39:07 +0200

    userContext shared with other components

--- 05-hook-app/src/components/09-useContext/HomeScreen.js
+++ 05-hook-app/src/components/09-useContext/HomeScreen.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './UserContext'
 
 export const HomeScreen = () => {
+	const userContext = useContext(UserContext)
+	console.log(userContext)
+
 	return (
 		<>
 			<h1>Home Screen</h1>
--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
-	return <AppRouter />
+	const user = {
+		id: 55240,
+		name: 'Camilo',
+		email: 'kamilo_vanegas@hotmail.com',
+	}
+
+	return (
+		<UserContext.Provider value={user}>
+			<AppRouter />
+		</UserContext.Provider>
+	)
 }
