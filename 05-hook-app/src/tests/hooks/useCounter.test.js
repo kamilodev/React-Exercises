fda4b5d312db340d495143b3974b6195c8fb5ee0
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-03 14:17:28 +0200

    Tests useCounter Completed

--- 05-hook-app/src/tests/hooks/useCounter.test.js
+++ 05-hook-app/src/tests/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks'
 import { useCounter } from '../../hooks/useCounter'
 
 describe('Pruebas en el Hook useCounter', () => {
@@ -8,5 +8,46 @@ describe('Pruebas en el Hook useCounter', () => {
 		expect(typeof result.current.increment).toBe('function')
 		expect(typeof result.current.decrement).toBe('function')
 		expect(typeof result.current.reset).toBe('function')
+	})
+
+	test('Debe tener el counter en 100', () => {
+		const { result } = renderHook(() => useCounter(100))
+		expect(result.current.counter).toBe(100)
+	})
+
+	test('Debe incrementar el counter en 1', () => {
+		const { result } = renderHook(() => useCounter(100))
+		const { increment } = result.current
+
+		act(() => {
+			increment()
+		})
+
+		const { counter } = result.current
+		expect(counter).toBe(101)
+	})
+
+	test('Debe decrementar el counter en 1', () => {
+		const { result } = renderHook(() => useCounter(100))
+		const { decrement } = result.current
+
+		act(() => {
+			decrement()
+		})
+
+		const { counter } = result.current
+		expect(counter).toBe(99)
+	})
+
+	test('Debe hacer un reset del counter y dejarlo en 10', () => {
+		const { result } = renderHook(() => useCounter())
+		const { reset } = result.current
+
+		act(() => {
+			reset()
+		})
+
+		const { counter } = result.current
+		expect(counter).toBe(10)
 	})
 })
