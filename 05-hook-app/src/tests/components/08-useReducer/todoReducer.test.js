ba4c2bcb9fe364d02ba02991e560ecf45cd7a7a0
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-07 22:30:35 +0200

    Test todoReducer Hook

--- 05-hook-app/src/tests/components/08-useReducer/todoReducer.test.js
+++ 05-hook-app/src/tests/components/08-useReducer/todoReducer.test.js
@@ -24,4 +24,26 @@ describe('Pruebas en todoReducer', () => {
 		expect(state.length).toEqual(3)
 		expect(state).toEqual([...demoTodos, newTodo])
 	})
+
+	test('Debe de borrar un TODO', () => {
+		const action = {
+			type: 'delete',
+			payload: 1,
+		}
+
+		const state = todoReducer(demoTodos, action)
+		expect(state.length).toBe(1)
+		expect(state).toEqual([demoTodos[1]])
+	})
+
+	test('Debe de cambiar el TOGGLE un TODO', () => {
+		const action = {
+			type: 'toggle',
+			payload: 1,
+		}
+
+		const state = todoReducer(demoTodos, action)
+		expect(state[0].done).toBe(true)
+		expect(state[1]).toEqual(demoTodos[1])
+	})
 })
