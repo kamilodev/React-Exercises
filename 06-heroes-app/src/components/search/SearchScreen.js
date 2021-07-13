0e6f6df8afbf1c281381cd0f810b0b95742952a2
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-13 06:05:50 +0200

    Implementing Filter for Search Component

--- 06-heroes-app/src/components/search/SearchScreen.js
+++ 06-heroes-app/src/components/search/SearchScreen.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import queryString from 'query-string'
 import { useLocation } from 'react-router-dom'
-import { heroes } from '../../data/heroes'
 import { useForm } from '../../hooks/useForm'
 import { HeroCard } from '../heroes/HeroCard'
+import { getHeroesByName } from '../../selectors/getHeroesByName'
 
 export const SearchScreen = ({ history }) => {
 	const location = useLocation()
@@ -14,7 +14,7 @@ export const SearchScreen = ({ history }) => {
 	})
 
 	const { searchText } = formValues
-	const heroesFiltered = heroes
+	const heroesFiltered = useMemo(() => getHeroesByName(q), [q])
 
 	const handleSearch = e => {
 		e.preventDefault()
@@ -49,6 +49,16 @@ export const SearchScreen = ({ history }) => {
 				<div className='col-7'>
 					<h4>Results</h4>
 					<hr />
+					{q === '' && (
+						<div className='alert alert-info'>Search a Hero</div>
+					)}
+
+					{q !== '' && heroesFiltered.length === 0 && (
+						<div className='alert alert-danger'>
+							There is not a hero with {q} name
+						</div>
+					)}
+
 					{heroesFiltered.map(hero => (
 						<HeroCard key={hero.id} {...hero} />
 					))}
