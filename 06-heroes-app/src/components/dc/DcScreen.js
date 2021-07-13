e81cf3c31a1e8daf766cf578a06138fc5b482c6b
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-13 03:17:29 +0200

    Creating Selectors for filter heroes by publisher and id

--- 06-heroes-app/src/components/dc/DcScreen.js
+++ 06-heroes-app/src/components/dc/DcScreen.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { HeroList } from '../heroes/HeroList'
 
 export const DcScreen = () => {
 	return (
-		<div>
+		<>
 			<h1>DC Screen</h1>
-		</div>
+			<hr />
+			<div>
+				<HeroList publisher={'DC Comics'} />
+			</div>
+		</>
 	)
 }
--- 06-heroes-app/src/components/marvel/MarvelScreen.js
+++ 06-heroes-app/src/components/marvel/MarvelScreen.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { HeroList } from '../heroes/HeroList'
 
 export const MarvelScreen = () => {
 	return (
-		<div>
+		<>
 			<h1>Marvel Screen</h1>
-		</div>
+			<hr />
+			<div>
+				<HeroList publisher={'Marvel Comics'} />
+			</div>
+		</>
 	)
 }
