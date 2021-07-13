6720b58c5f68bf6d62d45f6ec9de39b72bb59921
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-13 03:49:31 +0200

    MainScreen for heroes created

--- 06-heroes-app/src/components/heroes/HeroList.js
+++ 06-heroes-app/src/components/heroes/HeroList.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher'
+import { HeroCard } from './HeroCard'
 
 export const HeroList = ({ publisher }) => {
 	const heroes = getHeroesByPublisher(publisher)
 
 	return (
-		<ul>
+		<div className='row row-cols-1 row-cols-md-3 g-4'>
 			{heroes.map(hero => (
-				<li key={hero.id}>{hero.superhero}</li>
+				<HeroCard key={hero.id} {...hero}></HeroCard>
 			))}
-		</ul>
+		</div>
 	)
 }
