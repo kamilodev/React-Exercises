45421fc6ac49fbc245226e5c586cd0096a6737a2
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-13 02:41:52 +0200

    Inserting prop - push and replace - on routes

--- 06-heroes-app/src/components/login/LoginScreen.js
+++ 06-heroes-app/src/components/login/LoginScreen.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => {
+	const handleLogin = () => {
+		history.replace('/')
+	}
+
 	return (
-		<div>
-			<h1>Login Screen</h1>
+		<div className='container mt-5'>
+			<h1>Login</h1>
+			<hr />
+
+			<button className='btn btn-primary' onClick={handleLogin}>
+				Login
+			</button>
 		</div>
 	)
 }
