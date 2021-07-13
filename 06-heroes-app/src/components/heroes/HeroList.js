daac0c6570cdc64e9a76dc2b6a0b2fb3ec00010f
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-13 04:43:18 +0200

    Adding animations with animate.css in HeroScreen and HeroList

--- 06-heroes-app/src/components/heroes/HeroList.js
+++ 06-heroes-app/src/components/heroes/HeroList.js
@@ -6,7 +6,7 @@ export const HeroList = ({ publisher }) => {
 	const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher])
 
 	return (
-		<div className='row row-cols-1 row-cols-md-3 g-4'>
+		<div className='row row-cols-1 row-cols-md-3 g-4 animate__animated animate__fadeIn'>
 			{heroes.map(hero => (
 				<HeroCard key={hero.id} {...hero}></HeroCard>
 			))}
--- 06-heroes-app/src/components/heroes/HeroScreen.js
+++ 06-heroes-app/src/components/heroes/HeroScreen.js
@@ -28,10 +28,10 @@ export const HeroScreen = ({ history }) => {
 				<img
 					src={`../assets/heroes/${heroeId}.jpg`}
 					alt={superhero}
-					className='img-thumbnail'
+					className='img-thumbnail animate__animated animate__fadeInLeft'
 				/>
 			</div>
-			<div className='col-8'>
+			<div className='col-8 animate__animated animate__fadeIn'>
 				<h3>{superhero}</h3>
 				<ul className='list-group list-group-flush'>
 					<li className='list-group-item'>
