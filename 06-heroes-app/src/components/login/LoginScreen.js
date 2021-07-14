4da1e9a1755e8da68e11c1a8ce80be860c641748
Camilo Vanegas <kamilo_vanegas@hotmail.com>
2021-07-14 13:24:17 +0200

    Login User with Reducer

--- 06-heroes-app/src/components/login/LoginScreen.js
+++ 06-heroes-app/src/components/login/LoginScreen.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {AuthContext} from "../../auth/AuthContext";
+import {types} from "../../types/types";
 
 export const LoginScreen = ({ history }) => {
+
+	const {dispatch} = useContext(AuthContext)
+
 	const handleLogin = () => {
+
+		dispatch({
+			type: types.login,
+			payload: {
+				name: 'Camilo'
+			}
+		})
+
 		history.replace('/')
 	}
 
--- 06-heroes-app/src/components/ui/Navbar.js
+++ 06-heroes-app/src/components/ui/Navbar.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import './navbar.css'
+import {AuthContext} from "../../auth/AuthContext";
 
 export const Navbar = () => {
+	const {user: {name}} = useContext(AuthContext)
+
 	return (
 		<nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
 			<Link className='navbar-brand' to='/'>
@@ -39,6 +42,11 @@ export const Navbar = () => {
 
 			<div className='navbar-collapse collapse w-100 order-3 dual-collapse2'>
 				<ul className='navbar-nav'>
+
+					<span className='nav-item nav-link text-info'>
+						{name}
+					</span>
+
 					<NavLink
 						activeClassName='active'
 						className='nav-item nav-link'
