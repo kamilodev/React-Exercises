4608d8ccc93b0e4548a5fb19558485f38c8d395d
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-14 23:03:17 +0200

    Second Test for PrivateRoute

--- 06-heroes-app/src/test/routers/PrivateRoute.test.js
+++ 06-heroes-app/src/test/routers/PrivateRoute.test.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { PrivateRoute } from '../../routers/PrivateRoute'
 import { MemoryRouter } from 'react-router-dom'
 import { mount } from 'enzyme'
+import { DashboardRoutes } from '../../routers/DashboardRoutes'
 
 describe('Pruebas en <PrivateRoute />', () => {
+	const props = {
+		location: {
+			pathname: '/marvel',
+		},
+	}
+
 	test('Debe de mostrar el componente si esta autenticado y guarda el localStorage', () => {
-		const props = {
-			location: {
-				pathname: '/marvel',
-			},
-		}
+		Storage.prototype.setItem = jest.fn()
 
 		const wrapper = mount(
 			<MemoryRouter>
@@ -22,5 +25,21 @@ describe('Pruebas en <PrivateRoute />', () => {
 		)
 
 		expect(wrapper.find('span').exists()).toBe(true)
+		expect(localStorage.setItem).toHaveBeenCalledWith('lastPath', '/marvel')
+	})
+
+	test('Debe de bloquear el componente si no esta bloqueado', () => {
+		const wrapper = mount(
+			<MemoryRouter>
+				<PrivateRoute
+					isAutenticated={false}
+					component={() => <span>Holi!</span>}
+					{...props}
+				/>
+			</MemoryRouter>,
+		)
+
+		expect(wrapper.find('span').exists()).toBe(false)
+		expect(localStorage.setItem).toHaveBeenCalledWith('lastPath', '/marvel')
 	})
 })
