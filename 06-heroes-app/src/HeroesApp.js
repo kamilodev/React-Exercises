4439ec5068033e16c5f21256de33b2e08701b932
Camilo Vanegas <kamilo_vanegas@hotmail.com>
2021-07-14 12:52:45 +0200

    AuthContext Created

--- 06-heroes-app/src/HeroesApp.js
+++ 06-heroes-app/src/HeroesApp.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, {useReducer} from 'react'
 import { AppRouter } from './routers/AppRouter'
+import {AuthContext} from './auth/AuthContext'
+import {authReducer} from './auth/authReducer'
+
+const init = () => {
+	return JSON.parse(localStorage.getItem('user')) || {logged: false}
+}
 
 export const HeroesApp = () => {
-	return <AppRouter />
+	const [user, dispatch] = useReducer(authReducer, {}, init);
+	return (
+			<AuthContext.Provider value={{user, dispatch}}>
+				<AppRouter />
+			</AuthContext.Provider>
+		)
 }
