97444c406f322a92ea559742593cae241a57a274
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-14 20:31:25 +0200

    Logout routes protected

--- 06-heroes-app/src/routers/AppRouter.js
+++ 06-heroes-app/src/routers/AppRouter.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { LoginScreen } from '../components/login/LoginScreen'
 import { DashboardRoutes } from './DashboardRoutes'
+import { PrivateRoute } from './PrivateRoute'
+import { AuthContext } from '../auth/AuthContext'
 
 export const AppRouter = () => {
+	const { user } = useContext(AuthContext)
+	console.log(user)
+
 	return (
 		<Router>
 			<div>
 				<Switch>
 					<Route exact path='/login' component={LoginScreen} />
-					<Route path='/' component={DashboardRoutes} />
+					<PrivateRoute
+						path='/'
+						component={DashboardRoutes}
+						isAutenticated={user.logged}
+					/>
 				</Switch>
 			</div>
 		</Router>
---
+++ 06-heroes-app/src/routers/PrivateRoute.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { PropTypes } from 'prop-types'
+
+export const PrivateRoute = ({
+	isAutenticated,
+	component: Component,
+	...rest
+}) => {
+	return (
+		<Route
+			{...rest}
+			component={props =>
+				isAutenticated ? (
+					<Component {...props} />
+				) : (
+					<Redirect to='/login' />
+				)
+			}
+		/>
+	)
+}
+
+PrivateRoute.propTypes = {
+	isAutenticated: PropTypes.bool.isRequired,
+	component: PropTypes.func.isRequired,
+}
