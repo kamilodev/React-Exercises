1fd6dc4f7c934bea85fe6500713fb561211c2538
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-16 22:51:42 +0200

    Styling LoginScreen

--- 07-journal-app/src/components/auth/LoginScreen.js
+++ 07-journal-app/src/components/auth/LoginScreen.js
@@ -1,16 +1,46 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const LoginScreen = () => {
 	return (
 		<div>
-			<h3>Login</h3>
+			<h3 className='auth__title'>Login</h3>
 
 			<form action=''>
-				<input type='text' placeholder='email' name='email' />
-				<input type='password' placeholder='Password' name='password' />
-				<button type='submit'>Login</button>
-				<hr />
-				Google
+				<input
+					className='auth__input'
+					type='text'
+					placeholder='Email'
+					name='email'
+					autoComplete='off'
+				/>
+				<input
+					className='auth__input'
+					type='password'
+					placeholder='Password'
+					name='password'
+					autoComplete='off'
+				/>
+				<button type='submit' className='btn btn-primary btn-block'>
+					Login
+				</button>
+
+				<div className='auth__social-networks'>
+					<p>Login With Social Networks</p>
+					<div className='google-btn'>
+						<div className='google-icon-wrapper'>
+							<img
+								className='google-icon'
+								src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
+								alt='google button'
+							/>
+						</div>
+						<p className='btn-text'>
+							<b>Sign in with google</b>
+						</p>
+					</div>
+					<Link to='/auth/register'>Create new account</Link>
+				</div>
 			</form>
 		</div>
 	)
