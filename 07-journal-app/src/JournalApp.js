92616691265b35a778288b1e7e0e71c2cd5dbdb1
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-16 21:40:42 +0200

    Installig SASS and doing base configuration

--- 07-journal-app/src/JournalApp.js
+++ 07-journal-app/src/JournalApp.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import './styles/styles.scss'
+import { AppRouter } from './routers/AppRouter'
 
 export const JournalApp = () => {
-    return (
-        <div>
-            <h1>Journal App</h1>
-        </div>
-    )
+	return <AppRouter />
 }
