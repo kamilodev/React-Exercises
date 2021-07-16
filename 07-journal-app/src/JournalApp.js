e941e21727e7b8ace15583a07ad139cb96eff261
Camilo Vanegas <kamilo_vanegas@hotmail.com>
2021-07-16 13:28:44 +0200

    Start New Project Journal-App

---
+++ 07-journal-app/src/JournalApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles/styles.scss'
+
+export const JournalApp = () => {
+    return (
+        <div>
+            <h1>Journal App</h1>
+        </div>
+    )
+}
