fcca7abea8eabb9f63827db64efc53cb885b563d
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-17 14:15:11 +0200

    Create structure of HomeScreen

--- 07-journal-app/src/components/notes/NoteScreen.js
+++ 07-journal-app/src/components/notes/NoteScreen.js
@@ -5,6 +5,23 @@ export const NoteScreen = () => {
 	return (
 		<div className='notes__main-content'>
 			<NotesAppBar />
+			<div className='notes__content'>
+				<input
+					type='text'
+					placeholder='Some awesome title'
+					className='notes__title-input'
+					autoComplete='off'
+				/>
+				<textarea
+					placeholder='Some to remaind today?'
+					className='notes__textarea'></textarea>
+				<div className='notes__image'>
+					<img
+						src='http://unsplash.it/g/300?random&gravity=center'
+						alt='some kitties'
+					/>
+				</div>
+			</div>
 		</div>
 	)
 }
