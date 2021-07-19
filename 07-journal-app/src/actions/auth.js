1bead23385abce659da1486f3b83d52b973dc775
Camilo Vanegas <kamilo_vanegas@hotmail.com>
2021-07-19 13:33:37 +0200

    Login with email and password in firebase

--- 07-journal-app/src/actions/auth.js
+++ 07-journal-app/src/actions/auth.js
@@ -3,9 +3,16 @@ import { types } from '../types/types'
 
 export const startLoginEmailPassword = (email, password) => {
 	return dispatch => {
-		setTimeout(() => {
-			dispatch(login(123, 'Pedro'))
-		}, 3500)
+			firebase.auth().signInWithEmailAndPassword(email, password)
+				.then(({ user }) => {
+
+					dispatch(
+						login(user.uid, user.displayName)
+					)
+				})
+				.catch(e => {
+					console.log(e)
+				})
 	}
 }
 
