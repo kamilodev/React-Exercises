bd908ec4970f3555a3f8ce32df56884ca647c096
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-19 23:54:04 +0200

    Keep User Authentication in state

--- 07-journal-app/src/routers/AppRouter.js
+++ 07-journal-app/src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
 	BrowserRouter as Router,
 	Redirect,
@@ -7,8 +7,21 @@ import {
 } from 'react-router-dom'
 import { JournalScreen } from '../components/journal/JournalScreen'
 import { AuthRouter } from './AuthRouter'
+import { firebase } from '../firebase/firebase-config'
+import { useDispatch } from 'react-redux'
+import { login } from '../actions/auth'
 
 export const AppRouter = () => {
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged(user => {
+			if (user?.uid) {
+				dispatch(login(user.uid, user.displayName))
+			}
+		})
+	}, [])
+
 	return (
 		<Router>
 			<div>
