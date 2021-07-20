5d6893a907e8b7832d2a087abd978ad6150f6c8d
Camilo Vanegas <kamilo_vanegas@hotmail.com>
2021-07-20 08:47:41 +0200

    Logout of firebase, and clean state

--- 07-journal-app/src/components/journal/Sidebar.js
+++ 07-journal-app/src/components/journal/Sidebar.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import { JournalEntries } from './JournalEntries'
+import {useDispatch} from "react-redux";
+import {startLogout} from "../../actions/auth";
 
 export const Sidebar = () => {
+
+	const dispatch = useDispatch()
+	const handleLogout = () => {
+		dispatch(startLogout())
+	}
+
 	return (
 		<aside className='journal__sidebar'>
 			<div className='journal__sidebar-navbar'>
@@ -9,7 +17,12 @@ export const Sidebar = () => {
 					<i className='far fa-moon'></i>
 					<span> Kamilo</span>
 				</h3>
-				<button className='btn'>Logout</button>
+				<button
+					className='btn'
+					onClick={handleLogout}
+				>
+					Logout
+				</button>
 			</div>
 			<div className='journal__new-entry'>
 				<i className='far fa-calendar-plus fa-5x'></i>
