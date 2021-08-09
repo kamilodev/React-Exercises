9de52f9fcf057c5c9a8268fd50b5dc3d2ebe1698
kamilo923 <kamilo_vanegas@hotmail.com>
2021-08-09 21:12:54 +0200

    Show notes in sidebar

--- 07-journal-app/src/components/journal/JournalEntry.js
+++ 07-journal-app/src/components/journal/JournalEntry.js
@@ -1,25 +1,27 @@
 import React from 'react'
+import moment from 'moment'
 
-export const JournalEntry = () => {
+export const JournalEntry = ({ id, date, title, body, url }) => {
+	const noteDate = moment(date)
+	console.log(noteDate)
+
 	return (
 		<div className='journal__entry pointer'>
-			<div
-				className='journal__entry-picture'
-				style={{
-					backgroundSize: 'cover',
-					backgroundImage:
-						'url(https://amuraworld.com/images/articles/133-botswana/01-full/132-leon-africano.jpg)',
-				}}></div>
+			{url && (
+				<div
+					className='journal__entry-picture'
+					style={{
+						backgroundSize: 'cover',
+						backgroundImage: `url(${url})`,
+					}}></div>
+			)}
 			<div className='journal__entry-body'>
-				<p className='journal__entry-title'>Un nuevo día</p>
-				<p className='journal__entry-content'>
-					Duis elit incididunt amet enim ullamco occaecat enim aliquip
-					do.
-				</p>
+				<p className='journal__entry-title'>{title}</p>
+				<p className='journal__entry-content'>{body}</p>
 			</div>
 			<div className='journal__entry-date-box'>
-				<span>Monday</span>
-				<h4>28</h4>
+				<span> {noteDate.format('dddd')} </span>
+				<h4> {noteDate.format('Do')} </h4>
 			</div>
 		</div>
 	)
