4ae3aa483badde511f5a5fe4ee54145cc2e4b053
kamilo923 <kamilo_vanegas@hotmail.com>
2021-08-09 15:58:25 +0200

    Creating new note in cloud FireStore

--- 07-journal-app/src/components/journal/Sidebar.js
+++ 07-journal-app/src/components/journal/Sidebar.js
@@ -2,12 +2,17 @@ import React from 'react'
 import { JournalEntries } from './JournalEntries'
 import { useDispatch, useSelector } from 'react-redux'
 import { startLogout } from '../../actions/auth'
+import { startNewNote } from '../../actions/notes'
 
 export const Sidebar = () => {
 	const { name } = useSelector(state => state.auth)
 	const dispatch = useDispatch()
 	const handleLogout = () => {
 		dispatch(startLogout())
+	}
+
+	const handleAddNew = () => {
+		dispatch(startNewNote())
 	}
 
 	return (
@@ -21,7 +26,7 @@ export const Sidebar = () => {
 					Logout
 				</button>
 			</div>
-			<div className='journal__new-entry'>
+			<div className='journal__new-entry' onClick={handleAddNew}>
 				<i className='far fa-calendar-plus fa-5x'></i>
 				<p className='mt-5'>New Entry</p>
 			</div>
--- 07-journal-app/src/types/types.js
+++ 07-journal-app/src/types/types.js
@@ -7,4 +7,12 @@ export const types = {
 
 	uiStartLoading: '[UI] Start Loading',
 	uiFinishLoading: '[UI] Finish Loading',
+
+	notesAddNew: '[Notes] New Note',
+	notesActive: '[Notes] Active Note',
+	notesLoad: '[Notes] Load Note',
+	notesUpdated: '[Notes] Updated Note',
+	notesFileUrl: '[Notes] image url',
+	notesDelete: '[Notes] Delete Note',
+	notesLogoutCleaning: '[Notes] Cleaning Note',
 }
