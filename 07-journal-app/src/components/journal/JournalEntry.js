055edcb21fa976e05e76c534c9bd0905d491a952
kamilo923 <kamilo_vanegas@hotmail.com>
2021-08-09 21:58:57 +0200

    Change active note to change selection

--- 07-journal-app/src/components/journal/JournalEntry.js
+++ 07-journal-app/src/components/journal/JournalEntry.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import moment from 'moment'
+import { activeNote } from '../../actions/notes'
+import { useDispatch } from 'react-redux'
 
 export const JournalEntry = ({ id, date, title, body, url }) => {
 	const noteDate = moment(date)
-	console.log(noteDate)
+	const dispatch = useDispatch()
+
+	const handleEntryClick = () => {
+		dispatch(
+			activeNote(id, {
+				date,
+				title,
+				body,
+				url,
+			}),
+		)
+	}
 
 	return (
-		<div className='journal__entry pointer'>
+		<div className='journal__entry pointer' onClick={handleEntryClick}>
 			{url && (
 				<div
 					className='journal__entry-picture'
--- 07-journal-app/src/components/notes/NoteScreen.js
+++ 07-journal-app/src/components/notes/NoteScreen.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux'
+import { useForm } from '../../hooks/useForm'
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
+	const { active: note } = useSelector(state => state.notes)
+	const [formValues, handleInputChange, reset] = useForm(note)
+	const { body, title } = formValues
+
+	const activeId = useRef(note.id)
+
+	useEffect(() => {
+		if (note.id !== activeId.current) {
+			reset(note)
+			activeId.current = note.id
+		}
+	}, [note, reset])
+
 	return (
 		<div className='notes__main-content'>
 			<NotesAppBar />
@@ -11,16 +26,22 @@ export const NoteScreen = () => {
 					placeholder='Some awesome title'
 					className='notes__title-input'
 					autoComplete='off'
+					value={title}
+					onChange={handleInputChange}
 				/>
 				<textarea
 					placeholder='Some to remaind today?'
-					className='notes__textarea'></textarea>
-				<div className='notes__image'>
-					<img
-						src='http://unsplash.it/g/300?random&gravity=center'
-						alt='some kitties'
-					/>
-				</div>
+					className='notes__textarea'
+					value={body}
+					onChange={handleInputChange}></textarea>
+				{note.url && (
+					<div className='notes__image'>
+						<img
+							src='http://unsplash.it/g/300?random&gravity=center'
+							alt='some kitties'
+						/>
+					</div>
+				)}
 			</div>
 		</div>
 	)
