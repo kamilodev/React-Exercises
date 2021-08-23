4e2669a1070411bf7d1a6991d4cc040909633804
kamilo923 <kamilo_vanegas@hotmail.com>
2021-08-23 21:40:10 +0200

    Delete note from AppJournal and Firebase

--- 07-journal-app/src/components/notes/NoteScreen.js
+++ 07-journal-app/src/components/notes/NoteScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { activeNote } from '../../actions/notes'
+import { activeNote, startDeleting } from '../../actions/notes'
 import { useForm } from '../../hooks/useForm'
 import { NotesAppBar } from './NotesAppBar'
 
@@ -9,7 +9,7 @@ export const NoteScreen = () => {
 
 	const { active: note } = useSelector(state => state.notes)
 	const [formValues, handleInputChange, reset] = useForm(note)
-	const { body, title } = formValues
+	const { body, title, id } = formValues
 
 	const activeId = useRef(note.id)
 
@@ -23,6 +23,10 @@ export const NoteScreen = () => {
 	useEffect(() => {
 		dispatch(activeNote(formValues.id, { ...formValues }))
 	}, [formValues, dispatch])
+
+	const handleDelete = () => {
+		dispatch(startDeleting(id))
+	}
 
 	return (
 		<div className='notes__main-content'>
@@ -49,6 +53,10 @@ export const NoteScreen = () => {
 					</div>
 				)}
 			</div>
+
+			<button className='btn btn-danger' onClick={handleDelete}>
+				Delete
+			</button>
 		</div>
 	)
 }
