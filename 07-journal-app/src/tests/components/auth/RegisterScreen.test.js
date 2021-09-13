ff839cdc3a131291834786834b7186b00d4d6bc9
kamilo923 <kamilo_vanegas@hotmail.com>
2021-09-13 19:25:29 +0200

    Test in <RegisterScreen /> Component

---
+++ 07-journal-app/src/tests/components/auth/RegisterScreen.test.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { mount } from 'enzyme'
+import { RegisterScreen } from '../../../components/auth/RegisterScreen'
+import { Provider } from 'react-redux'
+import { MemoryRouter } from 'react-router-dom'
+
+import '@testing-library/jest-dom'
+import { types } from '../../../types/types'
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
+
+const initState = {
+	auth: {},
+	ui: {
+		loading: false,
+		msgError: null,
+	},
+}
+
+let store = mockStore(initState)
+
+const wrapper = mount(
+	<Provider store={store}>
+		<MemoryRouter>
+			<RegisterScreen />
+		</MemoryRouter>
+	</Provider>,
+)
+
+describe('Test in <RegisterScreen /> component', () => {
+	test('Sould display <RegisterScreen /> correctly', () => {
+		expect(wrapper).toMatchSnapshot()
+	})
+
+	test('Should dispatch respect action', () => {
+		const emailField = wrapper.find('input[name="email"]')
+
+		emailField.simulate('change', {
+			target: {
+				value: '',
+				name: 'email',
+			},
+		})
+
+		wrapper.find('form').simulate('submit', {
+			preventDefault() {},
+		})
+
+		const actions = store.getActions()
+
+		expect(actions[0]).toEqual({
+			type: types.uiSetError,
+			payload: 'Name is required',
+		})
+	})
+})
