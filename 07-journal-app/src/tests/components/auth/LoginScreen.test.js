0b07f2988f17d4773264c738d75189eac3cf3e1e
kamilo923 <kamilo_vanegas@hotmail.com>
2021-09-13 15:15:59 +0200

    Test in login and google login

--- 07-journal-app/src/tests/components/auth/LoginScreen.test.js
+++ 07-journal-app/src/tests/components/auth/LoginScreen.test.js
@@ -5,8 +5,17 @@ import { mount } from 'enzyme'
 import { LoginScreen } from '../../../components/auth/LoginScreen'
 import { Provider } from 'react-redux'
 import { MemoryRouter } from 'react-router-dom'
+import {
+	startGoogleLogin,
+	startLoginEmailPassword,
+} from '../../../actions/auth'
 
 import '@testing-library/jest-dom'
+
+jest.mock('../../../actions/auth', () => ({
+	startGoogleLogin: jest.fn(),
+	startLoginEmailPassword: jest.fn(),
+}))
 
 const middlewares = [thunk]
 const mockStore = configureStore(middlewares)
@@ -21,6 +30,8 @@ const initState = {
 
 let store = mockStore(initState)
 
+store.dispatch = jest.fn()
+
 const wrapper = mount(
 	<Provider store={store}>
 		<MemoryRouter>
@@ -32,9 +43,38 @@ const wrapper = mount(
 describe('Test in <LoginScreen />', () => {
 	beforeEach(() => {
 		store = mockStore(initState)
+		jest.clearAllMocks()
 	})
 
 	test('Should be display correctly', () => {
 		expect(wrapper).toMatchSnapshot()
+	})
+
+	test('Should dispatch startGoogleLogin action', () => {
+		wrapper.find('.google-btn').prop('onClick')()
+
+		expect(startGoogleLogin).toHaveBeenCalled()
+	})
+
+	test('Should dispatch startLogin with respective arguments', () => {
+		const email = 'kamilo923@me.com'
+		const password = 'KavDev923'
+
+		wrapper.find('input[name="email"]').simulate('change', {
+			target: {
+				name: 'email',
+				value: email,
+			},
+		})
+		wrapper.find('input[name="password"]').simulate('change', {
+			target: {
+				name: 'password',
+				value: password,
+			},
+		})
+
+		wrapper.find('form').prop('onSubmit')({ preventDefault() {} })
+
+		expect(startLoginEmailPassword).toHaveBeenCalledWith(email, password)
 	})
 })
